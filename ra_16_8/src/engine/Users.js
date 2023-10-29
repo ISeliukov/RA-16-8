cb0731f6afeb3c3301323f76b361fa95a305482e
Garysi <garysi@rambler.ru>
2023-10-29 14:01:08 +0300

    add ver 0.1

---
+++ ra_16_8/src/engine/Users.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { List } from "./List.js";
+import { Details } from "./Details.js";
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function Users() {
+  const [user, setUser] = useState({});
+
+  return (
+    <div className="users">
+      <List onSelectUser={setUser} select={user.id} />
+      {user.id ? <Details id={user.id} /> : null}
+    </div>
+  );
+}
+
+export { Users };
